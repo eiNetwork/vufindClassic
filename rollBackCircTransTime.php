f23788f0c38ff9d0a56241c504050241d38c750e
17orange <bradley.patton@gmail.com>
2018-08-22 14:53:00 -0400

    Failsafe to clear out memcache when the reindexing is finished.

--- rollBackCircTransTime.php
+++ rollBackCircTransTime.php
@@ -3,6 +3,9 @@
   $memcached = new Memcached();
   $memcached->addServer('localhost', 11211);
 
+  // clean it out
+  $memcached->flush();
+
   // grab the start time for our circ_trans query
   $circTransTime = strtotime("yesterday 20:00:00");
   $memcached->set("lastCircTransTime", strftime("%Y-%m-%d %T", $circTransTime));
